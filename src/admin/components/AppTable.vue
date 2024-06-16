af0b1cf539bcb75c5207ff013df222b877291f55
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-06-16 23:31:32 +0600

    WIP:: App Table Components

---
+++ src/admin/components/AppTable.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="tm-table-wrapper">
+        <div class="tm-table-header">
+            <h1 class="table-title">All Trips</h1>
+            <el-button size="large" type="primary" icon="Plus" >Add New Trip </el-button>
+        </div>
+        <div class="tm-table-filter">
+            <el-input class="tm-search-input" v-model="input1" style="width: 240px" size="large" placeholder="Please Input"
+                prefix-icon="Search" />
+            <AppDatePicker />
+        </div>
+        <el-table :data="tableData" height="250" style="width: 100%">
+            <el-table-column prop="date" label="Date" width="180" />
+            <el-table-column prop="name" label="Name" width="180" />
+            <el-table-column prop="address" label="Address" />
+        </el-table>
+    </div>
+</template>
+
+<script>
+import AppDatePicker from "@/components/element/AppDatePicker.vue"
+export default {
+    name: "AppTable",
+    components: {
+        AppDatePicker
+    },
+    data() {
+        return {
+            input1: "",
+            // Search: "el-icon-search"
+        };
+    },
+    props: {
+        tableData: {
+            type: Array,
+            default: () => [],
+            required: true
+        }
+    }
+}
+</script>
--- src/admin/pages/page_trips/AllTrips.vue
+++ src/admin/pages/page_trips/AllTrips.vue
@@ -1,3 +1,54 @@
 <template>
-   <h1> All Trips hiugu utuy ttuy </h1>
+    <AppTable :tableData="tableData"/>
 </template>
+
+<script>
+import AppTable from  "@/components/AppTable.vue";
+export default {
+    components: {
+        AppTable
+    },
+    data() {
+        return {
+            tableData: [
+                {
+                    date: '2016-05-03',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                    tag: 'Home',
+                },
+                {
+                    date: '2016-05-02',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                    tag: 'Office',
+                },
+                {
+                    date: '2016-05-04',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                    tag: 'Home',
+                },
+                {
+                    date: '2016-05-01',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                    tag: 'Office',
+                },
+            ]
+        }
+    }
+}
+</script>
