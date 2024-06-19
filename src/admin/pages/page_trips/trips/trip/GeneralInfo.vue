b541678d24f3c394afe49d69a2c9985b6973a370
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-06-20 01:04:42 +0600

    WIP:: Trip Editor

--- src/admin/pages/page_trips/trips/trip/GeneralInfo.vue
+++ src/admin/pages/page_trips/trips/trip/GeneralInfo.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1>General</h1>
+    <div class="tm-trip-general-section">
+        <h2 class="section-title">General</h2>
+        <div class="tm_form_wrapper">
+            <div class="input-wrapper">
+                <p class="form-label" for="name">Trip Title *</p>
+                <el-input v-model="trip_info.post_title" style="width: 100%" placeholder="Enter Trip Title" size="large" />
+                <!-- <p class="error-message">{{ slug_error }}</p> -->
+            </div>
+
+            <div class="input-wrapper">
+                <p class="form-label" for="name">Trip Code *</p>
+                <el-input v-model="meta.general.trip_code" style="width: 100%" placeholder="Enter Unique Trip Code , Ex: WTE-4606" size="large" />
+                <!-- <p class="error-message">{{ slug_error }}</p> -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,6 +22,11 @@ export default {
     data() {
         return {
             trip_id: null,
+            meta:{
+                general: {
+                    trip_code: ''
+                }
+            }
         }
     },
     props: {
