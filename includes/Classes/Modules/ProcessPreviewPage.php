633c112759a6b6766db879daeb8cba1e6c622cde
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-07-01 18:39:23 +0600

    Trip:: Preview Header

--- includes/Classes/Modules/ProcessPreviewPage.php
+++ includes/Classes/Modules/ProcessPreviewPage.php
@@ -23,10 +23,12 @@ class ProcessPreviewPage {
     public function renderPreview($template_id)
     {
         $template = get_post($template_id);
-
+      
         if (!$template || $template->post_type !== 'tm_trip') {
             return;
         }
+
+        $template_meta = get_post_meta($template_id, 'trip_meta', true);
         
         add_action('wp_enqueue_scripts', function () {
             wp_enqueue_style('travel_manager_public_css', TM_URL.'assets/css/tm_public.css', [], TM_VERSION);
@@ -34,9 +36,11 @@ class ProcessPreviewPage {
         });
 
         wp_head();
-        View::render('Trip.Template1', [
+        View::render('Preview.TripPreview', [
             'shortcode' => '[tm_trip id="' . $template_id . '"]',
-            'edit_url' => admin_url('admin.php?page=wp-travel-manager#/trip/'.$template_id . '/edit'),
+            'edit_url' => admin_url('admin.php?page=travel-manager.php#/trip/'.$template_id . '/edit'),
+            'title' => mb_strlen($template->post_title) > 30 ? mb_substr($template->post_title, 0, 30) . '...' : $template->post_title,
+            'trip' => $template_meta,
         ]);
         wp_footer();
         exit();
