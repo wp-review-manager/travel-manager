f9a37df46bed6d403deed5c2613b988e59e2d1ba
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-07-02 23:28:27 +0600

    Init:: Shortcode & Preview Template

--- includes/Classes/Routes/ShortcodeRegister.php
+++ includes/Classes/Routes/ShortcodeRegister.php
@@ -1,10 +1,37 @@
 <?php
 namespace WPTravelManager\Classes\Routes;
+use WPTravelManager\Classes\ArrayHelper as Arr;
+use WPTravelManager\Classes\View;
 
 class ShortcodeRegister {
     
     public function register()
     {
-        // your shortcode here
+        add_shortcode( 'tm_trip', array( $this, 'singleTripShortCode' ) );
     }
+    public function singleTripShortCode( $atts )
+    {
+        $id = Arr::get( $atts, 'id', 0 );
+        if( empty( $id ) ){
+            return;
+        }
+        $this->preparedRenderData( $id );
+        
+    }
+
+    public function preparedRenderData( $id )
+    {
+        $post = get_post( $id );
+        if( empty( $post ) ){
+            return;
+        }
+        $post_meta = get_post_meta( $id, 'trip_meta', true );
+        $post_meta = maybe_unserialize( $post_meta );
+
+        View::render('Preview/Template1',[
+            'id' => $id,
+            'title' => $post->post_title,
+        ]);
+    }
+        
 }
--- includes/Views/Preview/Template1.php
+++ includes/Views/Preview/Template1.php
@@ -1,3 +1,12 @@
-<h1>
-    sdkgfjsdgfjgsdjhfg
-</h1>
+<div class="tm_trip_one_shortcode_preview_wrapper">
+    <main class="tm_trip_main">
+        <article>
+            <div class="entry-header">
+                <h1 class="entry-title"><?php echo esc_html($title); ?></h1>
+            </div>
+        </article>
+    </main>
+    <div class="tm_trip_sidebar">
+        sidebar
+    </div>
+</div>
