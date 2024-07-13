5ca9bb38131c45f7db35c4efc2df79b69344f56a
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-07-14 01:41:12 +0600

    data show in preview

--- includes/Views/Components/CollapseFAQs.php
+++ includes/Views/Components/CollapseFAQs.php
@@ -2,22 +2,22 @@
 namespace WPTravelManager\Views\Components;
 use WPTravelManager\Classes\ArrayHelper as Arr;
 
-class Collapse {
-    public static function RenderCollapse($itinerary, $title = 'Itinerary' )
+class CollapseFAQs {
+    public static function RenderFAQsCollapse($faqs, $title = 'Itinerary' )
     {
         ob_start();
-        $itineraries = Arr::get($itinerary, 'options', []);
+        $faqs_option = Arr::get($faqs, 'options', []);
         ?>
          <div class="tm_collapse-container">
-            <h1 class="tm_section_title"><?php echo htmlspecialchars ($itinerary['title']);  ?></h1>
-            <?php foreach ($itineraries as $itinerary) : ?>
+            <h1 class="tm_section_title"><?php echo htmlspecialchars ($faqs['title']);  ?></h1>
+            <?php foreach ($faqs_option as $faqs_option) : ?>
             <div class="tm_collapse" >
                 <div class="tm_collapse-btn">
                     <span class="dashicons dashicons-location"></span>
-                    <p> <?php echo htmlspecialchars ($itinerary['title']);  ?> </p>
+                    <p> <?php echo htmlspecialchars ($faqs_option['title']);  ?> </p>
                 </div>
                 <div class="tm_collapse-content">
-                    <p> <?php echo htmlspecialchars ($itinerary['description']);  ?></p>
+                    <p> <?php echo htmlspecialchars ($faqs_option['description']);  ?></p>
                 </div>
             </div>
             <?php endforeach; ?>
--- includes/Views/Components/Tab.php
+++ includes/Views/Components/Tab.php
@@ -9,8 +9,23 @@ class Tab
     public static function RenderTab($trip)
     {
         ob_start();
-        $itinerary = Arr::get($trip, 'itinerary', []);
+        $overview_title = Arr::get($trip, 'general.description.section_title', null);
+        $overview_description = Arr::get($trip, 'general.description.description', null);
+        $trip_highlights_title = Arr::get($trip, 'general.description.trip_highlights.title', null);
+        $trip_highlights = Arr::get($trip, 'general.description.trip_highlights.options', null);
        
+        $itinerary = Arr::get($trip, 'itinerary', []);
+
+        $section_title = Arr::get($trip, 'inc_exc.section_title', []);
+        $inc_title = Arr::get($trip, 'inc_exc.includes.title', []);
+        $inc_option = Arr::get($trip, 'inc_exc.includes.services', []);
+        $exc_title = Arr::get($trip, 'inc_exc.excludes.title', []);
+        $exc_option = Arr::get($trip, 'inc_exc.excludes.services', []);
+        
+        $faqs = Arr::get($trip, 'faqs', []);
+
+        $map_title = Arr::get($trip, 'map.title', []);
+        // dd($map);
         ?>
         <div class="tm_tab_container">
             <ul class="tm_tab_menu">
@@ -23,39 +38,21 @@ class Tab
 
             <div class="tm_tab_content tm_overview_content active" id="overview">
                 <div class="tm_section_title">
-                    <h2>Overview</h2>
+                    <h2><?php echo esc_html( $overview_title ) ?></h2>
                 </div>
                 <div class="tm_overview_content">
-                    <p>Travel is the movement of people between relatively distant geographical locations, and can involve travel by foot, bicycle, automobile, train, boat, bus, airplane, or other means, with or without luggage, and can be one way or round trip. Travel can also include relatively short stays between successive movements. The origin of the word "travel" is most likely lost to history. The term "travel" may originate from the Old French word travail, which means 'work'. According to the Merriam Webster dictionary, the first known use of the word travel was in the 14th century..</p>
+                    <p><?php echo esc_html( $overview_description ) ?></p>
                 </div>
                 <div class="tm_section_title">
-                    <h2>Highlights</h2>
+                    <h2><?php echo esc_html( $trip_highlights_title ) ?></h2>
                 </div>
                 <div class="tm_overview_content">
+                <?php foreach ($trip_highlights as $highlight) : ?>
                     <div class="tm_overview_highlights">
                         <span class="dashicons dashicons-saved"></span>
-                        Travel is the movement
+                        <?php echo htmlspecialchars ($highlight['label']); ?>
                     </div>
-
-                    <div class="tm_overview_highlights">
-                        <span class="dashicons dashicons-saved"></span>
-                        Travel is the movement
-                    </div>
-
-                    <div class="tm_overview_highlights">
-                        <span class="dashicons dashicons-saved"></span>
-                        Travel is the movement
-                    </div>
-
-                    <div class="tm_overview_highlights">
-                        <span class="dashicons dashicons-saved"></span>
-                        Travel is the movement
-                    </div>
-
-                    <div class="tm_overview_highlights">
-                        <span class="dashicons dashicons-saved"></span>
-                        Travel is the movement
-                    </div>
+                    <?php endforeach; ?>
                 </div>
             </div>
 
@@ -65,22 +62,22 @@ class Tab
 
             <div class="tm_tab_content" id="cost">
                 <div class="tm_trip_cost_section">
-                    <h1 class="tm_section_title">Cost</h1>
+                    <h1 class="tm_section_title"><?php echo esc_html( $section_title ) ?></h1>
                     <div class="tm_trip_included_section">
-                        <h2 class="tm_sub_section_title">The Cost Includes</h2>
+                        <h2 class="tm_sub_section_title"><?php echo esc_html( $inc_title ) ?></h2>
                         <div class="tm_trip_included_content">
                             <ul>
-                                <?php foreach (range(1,10) as $i) : ?>
-                                    <li> <span class="dashicons dashicons-saved"></span>Pick-up or Drop-off service from and to Airport(in our own vehicle)</li>
+                                <?php foreach ($inc_option as $inc_option) : ?>
+                                    <li> <span class="dashicons dashicons-saved"></span><?php echo esc_html( $inc_option ) ?></li>
                                 <?php endforeach; ?>
                             </ul>
                         </div>
 
-                        <h2 style="margin-top: 20px" class="tm_sub_section_title">The Cost Exclude</h2>
+                        <h2 style="margin-top: 20px" class="tm_sub_section_title"><?php echo esc_html( $exc_title ) ?></h2>
                         <div class="tm_trip_exclude_content">
                             <ul>
-                                <?php foreach (range(1,10) as $i) : ?>
-                                    <li> <span class="dashicons dashicons-no-alt"></span>Pick-up or Drop-off service from and to Airport(in our own vehicle)</li>
+                                <?php foreach ($exc_option as $exc_option) : ?>
+                                    <li> <span class="dashicons dashicons-no-alt"></span><?php echo esc_html( $exc_option ) ?></li>
                                 <?php endforeach; ?>
                             </ul>
                         </div>
@@ -90,10 +87,11 @@ class Tab
             </div>
 
             <div class="tm_tab_content" id="faqs">
-                <?php echo Collapse::RenderCollapse('FAQs'); ?>
+                <?php echo CollapseFAQs::RenderFAQsCollapse($faqs); ?>
             </div>
 
             <div class="tm_tab_content" id="map">
+            <h1 class="tm_section_title"><?php echo esc_html( $map_title ) ?></h1>
             <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe></div>
             </div>
         </div>
