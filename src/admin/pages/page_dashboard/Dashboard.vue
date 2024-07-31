aa5ef72728e05ba804dee3b6032ad8d8f2a50696
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-08-01 01:25:44 +0600

    dashboard design

--- src/admin/pages/page_dashboard/Dashboard.vue
+++ src/admin/pages/page_dashboard/Dashboard.vue
@@ -1,3 +1,81 @@
 <template>
-    <h1>Dashboard</h1>
+    <div class="dashboard">
+        <h1 class="dash-title">Click Tracking Overview</h1>
+        <div class="horizontal"></div>
+
+        <div class="dash-details">
+            <div class="dash-item">
+                <div class="left-item">
+                    <h1>49</h1>
+                    <p>total Click</p>
+                </div>
+                <div class="right-item">
+                    <div class="icon">
+                    <Icon icon="tm-eye" />
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item">
+                <div class="left-item">
+                    <h1>49</h1>
+                    <p>total Click</p>
+                </div>
+                <div class="right-item">
+                    <div class="icon">
+                    <Icon icon="tm-eye" />
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item">
+                <div class="left-item">
+                    <h1>49</h1>
+                    <p>total Click</p>
+                </div>
+                <div class="right-item">
+                    <div class="icon">
+                    <Icon icon="tm-eye" />
+                    </div>
+                </div>
+            </div>
+            <div class="dash-item">
+                <div class="left-item">
+                    <h1>49</h1>
+                    <p>total Click</p>
+                </div>
+                <div class="right-item">
+                    <div class="icon">
+                    <Icon icon="tm-eye" />
+                    </div>.
+                   
+                </div>
+            </div>
+            
+        </div>
+
+    </div>
+
+    <div class="dash-overview ">
+            <div class="integration">
+                <h1 class="dash-title">Integration</h1>
+                <div class="horizontal"></div>
+
+            </div>
+            <div class="affiliate">
+                <h1 class="dash-title">Affiliate Statistic</h1>
+                <div class="horizontal"></div>
+
+            </div>
+        </div>
+
+
 </template>
+
+<script>
+import Icon from "@/components/Icons/AppIcon.vue";
+export default{
+    components:{
+        Icon
+    }
+}
+
+</script>
