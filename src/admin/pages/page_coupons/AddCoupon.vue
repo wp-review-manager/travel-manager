6b9e9e02009eaaa30bdea876a4981c3b708b845d
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-08-27 01:24:20 +0600

    coupon ui design

---
+++ src/admin/pages/page_coupons/AddCoupon.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="tm_coupon_container">
+        <!-- tm_coupon_header start-->
+        <div class="tm_coupon_header">
+            <div class="tm_coupon_right">
+                <div class="tm_coupon_abel">
+                    <label>Coupon Title</label>
+                </div>
+                <input type="text" autocomplete="off" placeholder="Coupon Title" class="tm_coupon_input">
+                <p class="tm_coupon_msg">The name of this discount</p>
+            </div>
+            <div class="tm_coupon_left">
+                <div class="tm_coupon_abel">
+                    <label>Coupon Code</label>
+                </div>
+                <input type="text" autocomplete="off" placeholder="Coupon Code" class="tm_coupon_input">
+                <p class="tm_coupon_msg">Enter a code for this discount, such as 10PERCENT. Only use alphabets and
+                    numbers as coupon code.</p>
+            </div>
+        </div>
+        <!-- tm_coupon_header end -->
+
+        <!-- tm_coupon_amount_header start -->
+        <div class="tm_coupon_amount_header">
+            <h1 class="tm_coupon_title">Amount</h1>
+            <div class="tm_coupon_header">
+                <div class="tm_coupon_right">
+                    <div class="tm_coupon_abel">
+                        <label>Discount Type</label>
+                    </div>
+                    <select autocomplete="off" placeholder="Discount Type" class="tm_coupon_input">
+                        <option>Fixed</option>
+                        <option>Percent</option>
+                    </select>
+                    <p class="tm_coupon_msg">The name of this discount</p>
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>Discount Amount / Percent</label>
+                    </div>
+                    <input type="number" autocomplete="off" placeholder="Discount Amount / Percent"
+                        class="tm_coupon_input">
+                    <p class="tm_coupon_msg">Enter the discount percentage. 10 = 10% </p>
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>Min Purchase Amount</label>
+                    </div>
+                    <input type="number" autocomplete="off" placeholder="Min Purchase Amount" class="tm_coupon_input">
+                    <p class="tm_coupon_msg">The minimum amount required to use this discount. Leave the field blank if
+                        there is no minimum.</p>
+                </div>
+            </div>
+        </div>
+        <!-- tm_coupon_amount_header end -->
+
+        <!-- Expiration start -->
+        <div class="tm_coupon_amount_header">
+            <h1 class="tm_coupon_title">Expiration</h1>
+            <div class="tm_coupon_header">
+                <div class="tm_coupon_right">
+                    <div class="tm_coupon_abel">
+                        <label>Limit Per User(Email Based)</label>
+                    </div>
+                    <input type="number" autocomplete="off" placeholder="Limit Per User(Email Based)"
+                        class="tm_coupon_input">
+                    <p class="tm_coupon_msg">Enter the usage limit for a single user / Leave '0' for no limitation.</p>
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>Start Date</label>
+                    </div>
+                    <input type="date" autocomplete="off" placeholder="Start Date" class="tm_coupon_input">
+                    <p class="tm_coupon_msg">Enter the start date for this discount code in the format of yyyy-mm-dd.
+                        For no start date, leave blank.</p>
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>End Date</label>
+                    </div>
+                    <input type="date" autocomplete="off" placeholder="End Date" class="tm_coupon_input">
+                    <p class="tm_coupon_msg">Enter the expiration date for this discount code in the format of
+                        yyyy-mm-dd. For no expiration, leave blank</p>
+                </div>
+            </div>
+        </div>
+        <!-- Expiration end -->
+
+        <!-- Additional Information's start -->
+        <div class="tm_coupon_amount_header">
+            <h1 class="tm_coupon_title">Additional Information's </h1>
+            <div class="tm_coupon_header">
+                <div class="tm_coupon_right">
+                    <div class="tm_coupon_abel">
+                        <label>Applicable Forms</label>
+                    </div>
+                    <select autocomplete="off" placeholder="Discount Type" class="tm_coupon_input">
+                        <option>Blank form #01</option>
+                        <option>Blank form #013 </option>
+                    </select>
+                    <p class="tm_coupon_msg">Leave blank for applicable for all payment forms</p>
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>Status</label>
+                    </div>
+                    <div class="">
+                        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                        <label for="vehicle1" style="padding-right: 50px; color: #2a2929; font-weight: 600;"> Active</label>
+                        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                        <label for="vehicle2" style=" color: #2a2929; font-weight: 600;"> Inactive</label>
+                    </div>
+                  
+                    
+                </div>
+                <div class="tm_coupon_left">
+                    <div class="tm_coupon_abel">
+                        <label>Stackable  </label>
+                    </div>
+                    <div class="">
+                        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                        <label for="vehicle1" style="padding-right: 50px; color: #2a2929; font-weight: 600;"> Yes</label>
+                        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                        <label for="vehicle2" style=" color: #2a2929; font-weight: 600;"> No</label>
+                    </div>
+                    <p class="tm_coupon_msg">Can this coupon code can be used with other coupon code</p>
+                </div>
+            </div>
+        </div>
+        <!-- Additional Information's end -->
+
+        <!--footer -->
+        <div class="coupon_footer">
+            <button type="button" class="tm_coupon_button" style="margin-right: 15px;">Cancel</button>
+            <button type="button" class="tm_coupon_button tm_save">Save Coupon</button>
+        </div>
+    </div>
+</template>
--- src/admin/pages/page_coupons/AllCoupon.vue
+++ src/admin/pages/page_coupons/AllCoupon.vue
@@ -1,3 +1,185 @@
 <template>
-    <h1>Hello Coupon</h1>dshudhujd
+    <div class="tm_destinations_wrapper">
+
+        <AppModal
+            :title="'Add New Coupon'"
+            :width="1000"
+            :showFooter="false"
+            ref="add_coupon_modal">
+            <template #body>
+                <AddCoupon />
+            </template>
+        </AppModal>
+
+        <AppTable :tableData="activities" v-loading="loading">
+            <template #header>
+                <h1 class="table-title">Available Coupons</h1>
+                <el-button @click="openCouponAddModal" size="large" type="primary" icon="Plus">Add New Coupon</el-button>
+            </template>
+
+             <template #filter>
+                <el-input @change="getActivities" class="tm-search-input" v-model="search" style="width: 240px" size="large"
+                    placeholder="Please Input" prefix-icon="Search" />
+            </template>
+           
+            <template #columns>
+                <el-table-column prop="id" label="ID" width="60" />
+                <el-table-column prop="trip_activity_name" label="Title" width="auto" />
+                <el-table-column prop="trip_activity_slug" label="Code" width="auto" />
+                <el-table-column prop="trip_activity_desc" label="Expire Date" width="auto" />
+                <el-table-column prop="trip_activity_desc" label="Status" width="auto" />
+               
+                <el-table-column label="Operations" width="120">
+                    <template #default="{ row }">
+                        <el-tooltip class="box-item" effect="dark" content="Click to edit activities" placement="top-start">
+                            <el-button @click="openUpdateActivitiesModal(row)" link type="primary" size="small">
+                                <Icon icon="tm-edit" />
+                            </el-button>
+                        </el-tooltip>
+                        <el-tooltip class="box-item" effect="dark" content="Click to delete activities" placement="top-start">
+                            <el-button @click="openDeleteActivitiesModal(row)" link type="primary" size="small">
+                                <Icon icon="tm-delete" />
+                            </el-button>
+                        </el-tooltip>
+                    </template>
+                </el-table-column>
+            </template>
+
+            <template #footer>
+                <el-pagination
+                    v-model:current-page="currentPage"
+                    v-model:page-size="pageSize"
+                    :page-sizes="[10, 20, 30, 40]"
+                    large
+                    :disabled="total_activities <= pageSize"
+                    background
+                    layout="total, sizes, prev, pager, next"
+                    :total="+total_activities"
+                    @size-change="getActivities"
+                    @current-change="getActivities"
+                />
+            </template>
+
+        </AppTable>
+
+        <AppModal
+            :title="'Update Activities'"
+            :width="1000"
+            :showFooter="false"
+            ref="update_activities_modal">
+            <template #body>
+                <AddActivities :activities_data="activity" />
+            </template>
+        </AppModal>
+
+        <AppModal
+            :title="'Delete Activities'"
+            :width="400"
+            :showFooter="false"
+            ref="delete_activities_modal">
+            <template #body>
+                <div class="delete-modal-body">
+                    <h1>Are you sure ?</h1>
+                    <p>You want to delete this activities</p>
+                </div>
+            </template>
+            <template #footer>
+                <div class="tm-modal-footer">
+                    <el-button @click="$refs.delete_activities_modal.handleClose()" type="default" size="medium">Cancel</el-button>
+                    <el-button @click="deleteActivities" type="primary" size="medium">Delete</el-button>
+                </div>
+            </template>
+        </AppModal>
+
+    </div>
 </template>
+
+<script>
+import AppTable from "@/components/AppTable.vue";
+import AppModal from "@/components/AppModal.vue";
+import AppDatePicker from "@/components/element/AppDatePicker.vue"
+import AddCoupon from "./AddCoupon.vue";
+import Icon from "@/components/Icons/AppIcon.vue"
+
+export default {
+    components: {
+        AppTable,
+        AppModal,
+        AppDatePicker,
+        AddCoupon,
+        Icon
+    },
+    data() {
+        return {
+            search: '',
+            activities: [],
+            activity: {},
+            total_activities: 0,
+            loading: false,
+            add_coupon_modal: true,
+            currentPage: 1,
+            pageSize: 10,
+            active_id: null
+        }
+    },
+
+    methods: {
+        getActivities() {
+            this.loading = true;
+            let that = this;
+            jQuery
+                .post(ajaxurl, {
+                    action: "tm_activities",
+                    route: "get_activities",
+                    per_page: this.pageSize,
+                    page: this.currentPage,
+                    search: that.search,
+                    tm_admin_nonce: window.wpTravelManager.tm_admin_nonce,
+                }).then((response) => {
+                    that.activities = response?.data?.data?.activities;
+                    that.total_activities = response?.data?.data?.total;
+                }).fail((error) => {
+                    console.log(error);
+                }).always(() => {
+                    that.loading = false;
+                })
+        },
+        deleteActivities() {
+            let that = this;
+            jQuery
+                .post(ajaxurl, {
+                    action: "tm_activities",
+                    route: "delete_activities",
+                    id: that.active_id,
+                    tm_admin_nonce: window.wpTravelManager.tm_admin_nonce,
+                }).then((response) => {
+                    that.getActivities();
+                    that.$refs.delete_activities_modal.handleClose();
+                }).fail((error) => {
+                    console.log(error);
+                })
+        },
+      
+        openCouponAddModal() {
+            this.$refs.add_coupon_modal.openModel();
+        },
+       
+        openDeleteActivitiesModal(row) {
+            this.active_id = row.id;
+            this.$refs.delete_activities_modal.openModel();
+        },
+        openUpdateActivitiesModal(row) {
+            this.activity = row;
+            this.$refs.update_activities_modal.openModel();
+        },
+        updateDataAfterNewAdd(new_activities) {
+            this.$refs.add_coupon_modal.handleClose();
+            this.activities.unshift(new_activities);
+        },
+    },
+       created() {
+        this.getActivities();
+    },
+  
+}
+</script>
