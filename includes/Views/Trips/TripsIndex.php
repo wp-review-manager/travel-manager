fb86eb906e1fd7de53c42b0ce65df394157cc23c
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-09-12 21:31:28 +0600

    wip:: trips index page design

--- includes/Views/Trips/TripsIndex.php
+++ includes/Views/Trips/TripsIndex.php
@@ -3,6 +3,7 @@
 namespace WPTravelManager\Views\Trips;
 
 $demoImage = TRM_URL . 'assets/images/girl.jpeg';
+$demoImage2 = TRM_URL . 'assets/images/sunflower.jpg';
 ?>
 <div class="trm_container">
     <div class="trm_content">
@@ -67,7 +68,7 @@ $demoImage = TRM_URL . 'assets/images/girl.jpeg';
                                     </div>
                                     <div class="trm_trip_budget">
                                         <div class="trm_trip_discount">
-                                            <span >14% Off</span>
+                                            <span>14% Off</span>
                                         </div>
                                         <div class="trm_price_holder">
                                             <span class="trm_actual_price">$3000</span>
@@ -77,13 +78,99 @@ $demoImage = TRM_URL . 'assets/images/girl.jpeg';
                                 </div>
                                 <div class="trm_trip_desc">
                                     The Tiananmen, a gate in the wall of the Imperial City, was built in 1415 during the Ming dynasty. In the 17th century, fighting between... </div>
-                                 </div>
+                            </div>
 
+                        </div>
+                        <div class="trm_trip_aval_time">
+                            <div class="trm_category_trip_inner">
+                                <span class="category-available-trip-text">Available through out the year:</span>
+                                <ul class="category-available-months">
+                                    <li>Jan</li>
+                                    <li>Feb</li>
+                                    <li>Mar</li>
+                                    <li>Apr</li>
+                                    <li>May</li>
+                                    <li>Jun</li>
+                                    <li>Jul</li>
+                                    <li>Aug</li>
+                                    <li>Sep</li>
+                                    <li>Oct</li>
+                                    <li>Nov</li>
+                                    <li>Dec</li>
+                                </ul>
+                            </div>
+                            <a href="#" class="trm_view_btn">View Details</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- =================================== -->
+                <div class="trm_category_trips">
+                    <div class="trm_trips_details">
+                        <div class="trm_trips_image">
+                            <a href="#">
+                                <img src="<?php echo $demoImage2 ?>">
+                            </a>
+                        </div>
+                        <div class="trm_category_trip_content">
+                            <div class="trm_category_title">
+                                <h2>
+                                    <a  href="#">Motel 6 Conyers GA</a>
+                                </h2>
+                            </div>
+                            <div class="trm_category_trip_detail">
+                                <div class="trm_trip_category_price">
+                                    <div class="trm_trip_desti">
+                                        <div class="trm_trip_loc">
+                                            <span class="icon dashicons dashicons-location"></span>
+                                            <span class="trm_name">Nepal</span>
+                                        </div>
+                                        <div class="trm_trip_dur">
+                                            <span class="icon dashicons dashicons-clock"></span>
+                                            <span class="trm_name">7Day</span>
+                                        </div>
+                                    </div>
+                                    <div class="trm_trip_budget">
+                                        <div class="trm_trip_discount">
+                                            <span>14% Off</span>
+                                        </div>
+                                        <div class="trm_price_holder">
+                                            <span class="trm_actual_price">$3000</span>
+                                            <span class="trm_striked_price">$3500</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="trm_trip_desc">
+                                    The Tiananmen, a gate in the wall of the Imperial City, was built in 1415 during the Ming dynasty. In the 17th century, fighting between... </div>
+                            </div>
+
+                        </div>
+                        <div class="trm_trip_aval_time">
+                            <div class="trm_category_trip_inner">
+                                <span class="category-available-trip-text">Available through out the year:</span>
+                                <ul class="category-available-months">
+                                    <li>Jan</li>
+                                    <li>Feb</li>
+                                    <li>Mar</li>
+                                    <li>Apr</li>
+                                    <li>May</li>
+                                    <li>Jun</li>
+                                    <li>Jul</li>
+                                    <li>Aug</li>
+                                    <li>Sep</li>
+                                    <li>Oct</li>
+                                    <li>Nov</li>
+                                    <li>Dec</li>
+                                </ul>
+                            </div>
+                            <a href="#" class="trm_view_btn">View Details</a>
                         </div>
                     </div>
                 </div>
 
             </div>
+            <!-- ======================================== -->
+             
 
         </div>
     </div>
