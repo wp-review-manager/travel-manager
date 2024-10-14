d5627f29e99ae8f66cfef8794cb67d3d3bbfbcf5
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2024-10-14 11:28:17 +0600

    Fix:: Fileupload conflict with lodesh

--- includes/Classes/AdminMenuHandler.php
+++ includes/Classes/AdminMenuHandler.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+
 namespace WPTravelManager\Classes;
+
 use WPTravelManager\Classes\Models\Settings;
 use WPTravelManager\Classes\Services\GeneralSettings;
 
 
-class AdminMenuHandler {
+class AdminMenuHandler
+{
     public function renderMenu()
     {
         add_action('admin_menu', function () {
@@ -57,6 +60,44 @@ class AdminMenuHandler {
                 'admin.php?page=travel-manager.php#/settings',
             );
         });
+
+    }
+
+    public function getInlineScript()
+    {
+        return "
+            function isLodash () {
+            
+            let isLodash = false;
+
+            // If _ is defined and the function _.forEach exists then we know underscore OR lodash are in place
+            if ( 'undefined' != typeof( _ ) && 'function' == typeof( _.forEach ) ) {
+
+                // A small sample of some of the functions that exist in lodash but not underscore
+                const funcs = [ 'get', 'set', 'at', 'cloneDeep' ];
+
+                // Simplest if assume exists to start
+                isLodash  = true;
+
+                funcs.forEach( function ( func ) {
+                    // If just one of the functions do not exist, then not lodash
+                    isLodash = ( 'function' != typeof( _[ func ] ) ) ? false : isLodash;
+                } );
+            }
+
+            if ( isLodash ) {
+                // We know that lodash is loaded in the _ variable
+                return true;
+            } else {
+                // We know that lodash is NOT loaded
+                return false;
+            }
+        };
+
+        if ( isLodash() ) {
+            _.noConflict();
+        }
+        ";
     }
 
     public function renderAdminPage()
@@ -85,18 +126,25 @@ class AdminMenuHandler {
         ));
 
         wp_localize_script('TM-script-boot', 'wpTravelManager', $TM);
-    // <router-link class="tm-menu-item" to="/">
-    //     Dashboard
-    // </router-link> 
-    // <router-link class="tm-menu-item" to="/bookings" >
-    //     Bookings
-    // </router-link>
-    // <router-link class="tm-menu-item" to="/customers" >
-    //     Customers
-    // </router-link>
-    // <router-link class="tm-menu-item" to="/enquiries" >
-    // Enquiries
-    // </router-link>
+
+        /*
+        * This script only for resolve the conflict of lodash and underscore js
+        * Resolved the issue of media uploader specially for image upload
+        */
+        wp_add_inline_script('TM-script-boot', $this->getInlineScript(), 'after');
+
+        // <router-link class="tm-menu-item" to="/">
+        //     Dashboard
+        // </router-link> 
+        // <router-link class="tm-menu-item" to="/bookings" >
+        //     Bookings
+        // </router-link>
+        // <router-link class="tm-menu-item" to="/customers" >
+        //     Customers
+        // </router-link>
+        // <router-link class="tm-menu-item" to="/enquiries" >
+        // Enquiries
+        // </router-link>
 
         echo '<div class="TM-admin-page" id="TM_app">
             <div class="tm-main-menu">
